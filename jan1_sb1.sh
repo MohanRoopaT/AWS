d4b6a85193625d14f00bb67e13d61e4b7db0f34a
MohanRoopaT <tharugu.mohanroopa>
2022-01-01 07:50:38 +0530

    msg of modified jan1_sb1.sh

--- jan1_sb1.sh
+++ jan1_sb1.sh
@@ -1,3 +1,5 @@
 iidjhsa
 creating a file in the sub branch1 in local repo
 need to push the changes to remote without mergeing to main
+
+adding code when the source is in staging area to get know the diff in the code through git diff command
